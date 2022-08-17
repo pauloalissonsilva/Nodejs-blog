0097802383aa577290bda053890af4b650987347
PAULO ALISSON <alyssonbbmp@gmail.com>
2022-08-17 01:25:10 -0300

    Upgrade config

--- app.js
+++ app.js
@@ -95,7 +95,7 @@
     app.use('/admin' , admin);
 
 // Outros
-    const PORT = process.env.PORT || 3001;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
         console.log('Servidor rodando com sucesso!')
     });
